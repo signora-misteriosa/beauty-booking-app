341bff539da746f30c59d2a410921304384635e7
Eligia Raileanu <138327749+signora-misteriosa@users.noreply.github.com>
2026-04-07 14:07:12 +0100

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+
+    dependencies {
+        classpath ("com.android.tools.build:gradle:8.1.0")
+        classpath ("com.google.gms:google-services:4.3.15")
+    }
+}
+
+
+
+plugins {
+    alias(libs.plugins.android.application) apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
+}
+
